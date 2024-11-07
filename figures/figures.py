4f9e24f9896be71cbe81a1c1f1063a98d159c136
Vladislav Gaar <gaarvlad@gmail.com>
2024-11-07 13:03:18 +0300

    Fixed square's __str__ method

--- figures/figures.py
+++ figures/figures.py
@@ -61,7 +61,7 @@ class Square(Rectangle):
 
     def __str__(self):
         return (
-            f"{super(Shape, self).__str__()}, со стороной {self.side},"
+            f"{super(Rectangle, self).__str__()}, со стороной {self.side},"
             f" с площадью {self.area()} и периметром {self.perimeter()}"
         )
 
