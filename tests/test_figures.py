3d2c8e43f9b3d79645d35432239ccf160e34266a
дмитрий губковский <gubkovskij.dv@edu.spbstu.ru>
2024-11-20 10:27:11 +0300

    Revert "Добавил примеры в README"
    This reverts commit 33127e95d3803e0716c6c8e6766712bc8f27df3f.

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -14,13 +14,6 @@ def test_square():
     assert probationers_2.area() == 9
     assert probationers_2.perimeter() == 12
 
-    probationers_2.width = 2
-    assert probationers_2.area() == 4
-    assert probationers_2.perimeter() == 8
-
-    probationers_2.height = 5
-    assert probationers_2.area() == 25
-    assert probationers_2.perimeter() == 20
 
 def test_liskov():
     probationers_1 = Rectangle(3, 3)
